5bb6ac65607a6b53b6ec81c68555a4fa09fa8886
Raj Kumar Raju <rajkumarraju60433@gmail.com>
2025-12-05 19:07:25 +0530

    Initial commit

--- src/utils/newsApi.js
+++ src/utils/newsApi.js
@@ -1,5 +1,5 @@
 export async function fetchNews(language) {
-  const API_KEY = "pub_73688e7d4e6789934302705d47e31f7b785a0"; // Replace with your key other account :  pub_73688e7d4e6789934302705d47e31f7b785a0  other 2: pub_04ce5c5c6a984111ad88db9b8576b3f1
+  const API_KEY = "pub_04ce5c5c6a984111ad88db9b8576b3f1"; // Replace with your key other account :  pub_73688e7d4e6789934302705d47e31f7b785a0  other 2: pub_04ce5c5c6a984111ad88db9b8576b3f1
 
   const url = `https://newsdata.io/api/1/news?apikey=${API_KEY}&language=${language}`;
 
